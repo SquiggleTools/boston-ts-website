9863ab44bfedd6d7d754788c46e6e23f4cc37246
Josh Goldberg <git@joshuakgoldberg.com>
2024-03-05 15:53:59 -0500

    Added meta

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,21 @@
+import { MetaFunction } from "@remix-run/node";
+
 import { Layout } from "~/components/Layout";
+
+export const meta: MetaFunction = () => {
+	return [
+		{ title: "Boston TS Club" },
+		{
+			content: "Boston TS Club",
+			property: "og:title",
+		},
+		{
+			content:
+				"A meetup for JavaScript and TypeScript developers in the Boston, MA area.",
+			name: "description",
+		},
+	];
+};
 
 export default function Index() {
 	return <Layout title={["Boston", "TS Club"]}>Hello, world!</Layout>;
