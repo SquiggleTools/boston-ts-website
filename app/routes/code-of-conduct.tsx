fe93c52e68b85d6181374623f7f70be7fb46bfe9
Josh Goldberg <git@joshuakgoldberg.com>
2024-03-05 15:55:43 -0500

    Added more meta to pages

--- app/routes/code-of-conduct.tsx
+++ app/routes/code-of-conduct.tsx
@@ -1,4 +1,21 @@
+import { MetaFunction } from "@remix-run/node";
+
 import { Layout } from "~/components/Layout";
+import { description, title } from "~/constants";
+
+export const meta: MetaFunction = () => {
+	return [
+		{ title: `Code of Conduct | ${title}` },
+		{
+			content: description,
+			name: "description",
+		},
+		{
+			content: title,
+			property: "og:title",
+		},
+	];
+};
 
 export default function About() {
 	return (
