9ef8f47ce6dd2068c983e233beea45e6cc69b238
Josh Goldberg <git@joshuakgoldberg.com>
2024-03-07 12:42:59 -0500

    Add vite.config.ts and .vercel

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { vitePlugin as remix } from "@remix-run/dev";
+import { installGlobals } from "@remix-run/node";
+import { defineConfig } from "vite";
+import { vercelPreset } from "@vercel/remix/vite";
+
+installGlobals();
+
+export default defineConfig({
+	plugins: [
+		remix({
+			presets: [vercelPreset()],
+		}),
+	],
+});
