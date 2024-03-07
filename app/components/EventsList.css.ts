18bafb8fbe13cf1f9516aefd46747dcf1fed0b70
Josh Goldberg <git@joshuakgoldberg.com>
2024-03-07 11:41:59 -0500

    fix: only apply margin-top in small viewports

--- app/components/EventsList.css.ts
+++ app/components/EventsList.css.ts
@@ -1,5 +1,13 @@
 import { style } from "@vanilla-extract/css";
 
+import { breakpoints } from "~/styles.css";
+
 export const heading = style({
+	"@media": {
+		[breakpoints.medium]: {
+			marginTop: "0",
+		},
+	},
+
 	margin: "3rem 0",
 });
