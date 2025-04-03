d84214ddfc439b8f0e5af0bfec665955e11edfba
Jérémie Astori <jeremie@astori.fr>
2025-04-03 03:41:48 -0400

    feat: update copy of speak page

--- app/routes/speak.tsx
+++ app/routes/speak.tsx
@@ -34,14 +34,14 @@ export default function About() {
 			<BodyText>All we ask is:</BodyText>
 			<UnorderedList>
 				<li>
-					Send us your slides 24 hours in advance, so we can check them for
-					accessibility and/or dry run your talk with us the week before
+					Present a dry run of your talk the week before, so we can check it for
+					accessibility issues and make sure it runs the expected duration
 				</li>
 				<li>Arrive at the meetup before it starts</li>
 				<li>Be prepared to chat about your talk with attendees</li>
 				<li>
-					Optional: Provide us with an avatar, name, description, and URL
-					(social, website, etc.)
+					Provide us with a name, talk description, talk title, and optionally:
+					avatar, personal URL, and any social media profiles to share
 				</li>
 			</UnorderedList>
 			<BodyText>
